ab746043a8021860c82a5de21961fd9b1e86d674
unknown <mdrummanmondal2@gmail.com>
2026-05-15 00:25:32 +0600

    get started

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(express());
+app.use(cors());
+
+const uri = "mongodb+srv://etuitionbd:RHqMyzuXBaEL0Md4@simple-crud-cluster.0hdbxiy.mongodb.net/?appName=Simple-crud-cluster";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
